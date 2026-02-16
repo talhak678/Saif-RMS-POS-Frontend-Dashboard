b1edb8e0f2d4f19fb3b904f86d16a09ab55388bd
hasanhayat <aylskhsmvz.v@gmail.com>
2026-02-16 20:04:39 +0500

    dark

--- src/app/(admin)/role/page.tsx
+++ src/app/(admin)/role/page.tsx
@@ -47,7 +47,7 @@ export default function Roles() {
     return (
         <div className="min-h-screen">
             {/* Header */}
-            <div className="p-2 md:p-4 dark:bg-gray-900">
+            <div className="p-2 md:p-4 dark:bg-gray-900 dark:text-gray-200">
                 <div className="md:flex items-center justify-between mb-6">
                     <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-300">
                         Roles
