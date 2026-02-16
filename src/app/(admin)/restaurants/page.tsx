e18750ea6938a57aa87012ec737ea3b7a84d8182
hasanhayat <aylskhsmvz.v@gmail.com>
2026-02-16 19:55:52 +0500

    ui

--- src/app/(admin)/restaurants/page.tsx
+++ src/app/(admin)/restaurants/page.tsx
@@ -58,7 +58,7 @@ export default function RestaurantsPage() {
     };
 
     return (
-        <div className="min-h-screen p-3 md:p-6 dark:bg-gray-900">
+        <div className="min-h-screen p-3 md:p-6 dark:bg-gray-900 dark:text-gray-200">
             <div className="md:flex gap-1 items-center justify-between mb-6">
 
                 <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-200 mb-6">
