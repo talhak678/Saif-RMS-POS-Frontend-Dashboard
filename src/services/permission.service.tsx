0d118da0b62260479b09a77a2a4aabab56177fed
hasanhayat <aylskhsmvz.v@gmail.com>
2026-02-21 23:21:07 +0500

    url

--- src/services/permission.service.tsx
+++ src/services/permission.service.tsx
@@ -35,8 +35,32 @@ export const AuthProvider = ({ children }: { children: any }) => {
             setLoadingUser(true)
             const res = await authServ.getCurrentUser();
             if (res?.success) {
-                setUser(res?.data);
-                setPermissions(res?.data?.role?.permissions || []);
+                const userData = res?.data;
+                const isSuperAdminOnly = process.env.NEXT_PUBLIC_IS_SUPER_ADMIN_ONLY === 'true';
+                const isSuperAdmin = userData?.role?.name === 'SUPER_ADMIN';
+
+                // 1. Super Admin Exclusivity Check
+                if (isSuperAdminOnly && !isSuperAdmin) {
+                    toast.error('This url is for support team.');
+                    authServ.logout();
+                    return;
+                }
+                //check on normal url superadmin cannot login
+                if (!isSuperAdminOnly && isSuperAdmin) {
+                    toast.error('You are not authorized to access this url.');
+                    authServ.logout();
+                    return;
+                }
+
+                // 2. Restaurant Status Check
+                if (userData?.restaurant && userData.restaurant.status !== 'ACTIVE') {
+                    toast.error(`Your Restaurent is ${userData.restaurant.status.toLowerCase()}. Please contact support.`);
+                    authServ.logout();
+                    return;
+                }
+
+                setUser(userData);
+                setPermissions(userData?.role?.permissions || []);
             }
             else {
                 toast.error(res?.message || 'Failed to fetch user details')
