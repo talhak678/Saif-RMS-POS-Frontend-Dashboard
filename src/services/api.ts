ce9305000c6e611b923fed310bb81ac5a21294a0
huzaifa-codes <huzaifabusiness60@gmail.com>
2026-02-21 16:25:17 +0500

    changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { AuthServiceInstance } from "./auth.service";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "https://saif-rms-pos-frontend-dashboard.vercel.app/api",
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "https://saif-rms-pos-backend.vercel.app/api",
     withCredentials: true
 });
 
