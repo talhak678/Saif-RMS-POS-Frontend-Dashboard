0f881e63d836b55f525c5fddea2e4800f018f214
hasanhayat <aylskhsmvz.v@gmail.com>
2026-02-22 01:24:50 +0500

    plan

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { AuthServiceInstance } from "./auth.service";
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL || "https://saif-rms-pos-backend.vercel.app/api",
-        // baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api",
+    // baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/api",
 
     withCredentials: true
 });
